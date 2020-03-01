fa9a6d9a78c6d55bdeb9b765f43e89811ce8e946
Nguyenth306 <nguyenth306@gmail.com>
2020-03-01 00:24:59 -0600

    description

---
+++ WordCount.java
@@ -0,0 +1,9 @@
+public class WordCount{
+   public static void main (String[] args){
+      String s;
+   }
+   public int WordCount(String s){
+      String[] list = s.split(" ");
+      return (list.length);
+   }
+}
